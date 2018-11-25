1434d59a280e84b9985e0a1e85ec51c64ccc2c78
Unknown <JackPriceBurns@outlook.com>
2018-11-25 15:42:41 +0000

    Adds pieces to chess board

--- app/Classes/Managers/BoardManager.js
+++ app/Classes/Managers/BoardManager.js
@@ -1,6 +1,17 @@
 class BoardManager {
 
     constructor() {
+        this.defaultRow = 'RNBQKBNR';
+
+        this.pieceMap = {
+            R: 'rook',
+            N: 'knight',
+            B: 'bishop',
+            Q: 'queen',
+            K: 'king',
+            P: 'pawn',
+        };
+
         this.newBoard();
     }
 
@@ -11,11 +22,28 @@ class BoardManager {
             let row = [];
 
             for (let y = 0; y < 8; y++) {
-                row.push({piece: 'bishop', white: true});
+                let piece = null;
+                let white = false;
+
+                if (y === 0 || y === 7) {
+                    piece = this.pieceMap[this.defaultRow[x]];
+                }
+
+                if (y === 1 || y === 6) {
+                    piece = this.pieceMap.P;
+                }
+
+                if (y >= 6) {
+                    white = true
+                }
+
+                row.push({piece: piece, white: white});
             }
 
             this.board.push(row);
         }
+
+        console.table(this.board);
     }
 
     getBoard() {

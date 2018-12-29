59b15e661e88fa08bb7b680531d19b3275526a95
Jack Price-Burns <JackPriceBurns@outlook.com>
2018-12-29 11:04:59 +0000

    Refactor generating a chess board

--- app/Classes/Managers/BoardManager.js
+++ app/Classes/Managers/BoardManager.js
@@ -23,27 +23,37 @@ class BoardManager {
         this.board = [];
 
         for (let y = 0; y < 8; y++) {
-            let row = [];
+            this.board.push(this.generateRow(y));
+        }
+    }
 
-            for (let x = 0; x < 8; x++) {
-                let name = null;
+    /**
+     * Generate a single row on the chess board.
+     *
+     * @param yPos
+     * @returns {Array}
+     */
+    generateRow(yPos) {
+        let row = [];
 
-                if (y === 0 || y === 7) {
-                    name = this.nameMap[this.defaultRow[x]];
-                }
+        for (let x = 0; x < 8; x++) {
+            let name = null;
 
-                if (y === 1 || y === 6) {
-                    name = this.nameMap.P;
-                }
-
-                let piece = name ? new Piece(name, y <= 5) : null;
-                let location = {x, y};
-
-                row.push({piece, location});
+            if (yPos === 0 || yPos === 7) {
+                name = this.nameMap[this.defaultRow[x]];
             }
 
-            this.board.push(row);
+            if (yPos === 1 || yPos === 6) {
+                name = this.nameMap.P;
+            }
+
+            let piece = name ? new Piece(name, yPos <= 5) : null;
+            let location = {x, y: yPos};
+
+            row.push({piece, location});
         }
+
+        return row;
     }
 
     /**

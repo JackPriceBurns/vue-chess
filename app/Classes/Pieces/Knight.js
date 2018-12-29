e0323e1163acfaf225b7fcf6d97996abad1a41e9
Jack Price-Burns <JackPriceBurns@outlook.com>
2018-12-29 11:37:22 +0000

    Add movement logic for knights

--- app/Classes/Pieces/Knight.js
+++ app/Classes/Pieces/Knight.js
@@ -1,5 +1,50 @@
 class Knight {
+    constructor(piece, board) {
+        this.piece = piece;
+        this.board = board;
+    }
 
+    validMove(from, to) {
+        let yDiff = Math.abs(to.y - from.y);
+        let xDiff = Math.abs(to.x - from.x);
+
+        if (!Knight.isValid(yDiff, xDiff)) {
+            return false;
+        }
+
+        let defendingPiece = this.getDefendingPiece(to);
+
+        if (!defendingPiece) {
+            return true;
+        }
+
+        return defendingPiece.white !== this.piece.white;
+    }
+
+    /**
+     * Given the displacement in x and y check if this is valid knight movement.
+     *
+     * @param yDiff
+     * @param xDiff
+     * @returns {boolean}
+     */
+    static isValid(yDiff, xDiff) {
+        if (yDiff === 1 && xDiff === 2) {
+            return true;
+        }
+
+        return yDiff === 2 && xDiff === 1;
+    }
+
+    /**
+     * Given that you have a location get the piece at that location.
+     *
+     * @param to
+     * @returns {*|null}
+     */
+    getDefendingPiece(to) {
+        return this.board[to.y][to.x].piece;
+    }
 }
 
 module.exports = Knight;
